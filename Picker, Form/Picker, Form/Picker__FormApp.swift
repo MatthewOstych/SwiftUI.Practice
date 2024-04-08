aaca166a76f2f113a4a1d62f4c857da6208f07e1
Matvey Lantsov <155112942+MatthewOstych@users.noreply.github.com>
2024-04-08 22:25:40 +0300

    Add files via upload

---
+++ Picker, Form/Picker, Form/Picker__FormApp.swift
@@ -0,0 +1,17 @@
+//
+//  Picker__FormApp.swift
+//  Picker, Form
+//
+//  Created by Mathew Lantsov on 08.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct Picker__FormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
